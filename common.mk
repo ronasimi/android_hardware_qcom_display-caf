1639041f8e7d4dcf3b415a8a3d7cbf4d67477131
Ron Asimi <ron.asimi@gmail.com>
2014-09-03 23:14:16 -0400

    unused parameter fix
    Change-Id: I18a96d3020917090399502b9fe46387975299a36

--- common.mk
+++ common.mk
@@ -19,7 +19,7 @@ common_header_export_path := qcom/display
 common_libs := liblog libutils libcutils libhardware
 
 #Common C flags
-common_flags := -DDEBUG_CALC_FPS -Wno-missing-field-initializers -Wno-strict-aliasing
+common_flags := -DDEBUG_CALC_FPS -Wno-missing-field-initializers -Wno-strict-aliasing -Wno-error=unused-parameter
 
 ifeq ($(ARCH_ARM_HAVE_NEON),true)
     common_flags += -D__ARM_HAVE_NEON
